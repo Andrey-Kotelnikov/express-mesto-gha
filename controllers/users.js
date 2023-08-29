9650f8c3872c876d7d6f367ac86e48b67a721fe6
Andrey Kotelnikov <dobreutore@yandex.ru>
2023-08-29 20:50:03 +0300

    fix: test2

--- controllers/users.js
+++ controllers/users.js
@@ -1,30 +1,5 @@
 const User = require('../models/user');
 
-/*class ValidationError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = "ValidationError";
-    this.statusCode = 400;
-  }
-}
-
-class CastError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = "CastError";
-    this.statusCode = 404;
-  }
-}
-
-class ServerError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = "ServerError";
-    this.statusCode = 500;
-  }
-}*/
-
-
 // Получение всех пользователей
 module.exports.getUsers = (req, res) => {
   User.find({})
@@ -43,7 +18,7 @@ module.exports.getUserById = (req, res) => {
       res.send({ data: user })
     })
     .catch(err => {
-      if (err.name === 'ValidationError') {
+      if (err.name === 'AssertionError') {
         res.status(400).send({
           message: `${Object.values(err.errors).map((err) => err.message).join(', ')}`
         });
